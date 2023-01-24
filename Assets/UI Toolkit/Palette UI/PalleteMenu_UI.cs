1ceb4781a10c5a8e3f50401a2154cdd8015a158d
Theo Sanden <theo.sanden@edu.futuregames.nu>
2023-01-24 17:55:10 +0100

    Added small UI Element

--- Assets/UI Toolkit/Palette UI/PalleteMenu_UI.cs
+++ Assets/UI Toolkit/Palette UI/PalleteMenu_UI.cs
@@ -10,7 +10,7 @@ public class PalleteMenu_UI : MonoBehaviour
     [SerializeField] UIDocument uiDocument;
 
     Label label;
-    Button startButton;
+    Button start, left, right;
     private void Step(int step)
     {
         step = (step < 0) ? -1 : (step > 0) ? 1 : 0;
@@ -20,8 +20,9 @@ public class PalleteMenu_UI : MonoBehaviour
     {
         label.text = storage.GetCurrentPallette().Name;
         Color uiColor = storage.GetCurrentPallette().Palette.GetPixel(4, 0);
-        label.style.color = uiColor;
-        startButton.style.color = uiColor;
+        label.style.color = start.style.color = left.style.color = right.style.color = uiColor;
+
+
     }
     private void OnEnable()
     {
@@ -35,15 +36,15 @@ public class PalleteMenu_UI : MonoBehaviour
     }
     private void Bind()
     {
-        var leftButton = uiDocument.rootVisualElement.Q<Button>("Left");
-        var rightButton = uiDocument.rootVisualElement.Q<Button>("Right");
+        left = uiDocument.rootVisualElement.Q<Button>("Left");
+        right = uiDocument.rootVisualElement.Q<Button>("Right");
         label = uiDocument.rootVisualElement.Q<Label>("PaletteName");
-        leftButton.clicked += OnClickedLeftButton;
-        rightButton.clicked += OnClickedRightButton;
+        left.clicked += OnClickedLeftButton;
+        right.clicked += OnClickedRightButton;
 
         //Move this
-        startButton = uiDocument.rootVisualElement.Q<Button>("Start");
-        startButton.clicked += MoveThisIntoMainMenuScriptInstead;
+        start = uiDocument.rootVisualElement.Q<Button>("Start");
+        start.clicked += MoveThisIntoMainMenuScriptInstead;
     }
     private void UnBind()
     {
@@ -53,8 +54,8 @@ public class PalleteMenu_UI : MonoBehaviour
         rightButton.clicked -= OnClickedRightButton;
 
         //Move this
-        startButton = uiDocument.rootVisualElement.Q<Button>("Start");
-        startButton.clicked -= MoveThisIntoMainMenuScriptInstead;
+        start = uiDocument.rootVisualElement.Q<Button>("Start");
+        start.clicked -= MoveThisIntoMainMenuScriptInstead;
     }
     void OnClickedLeftButton()
     {
